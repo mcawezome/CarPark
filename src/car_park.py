1fc97cdae3ccc8d8f5540b8efdaba45fae3f5366
mcawezome <20085898@tafe.wa.edu.au>
2024-11-07 14:09:41 +0800

    Added __str__() to CarPark class

--- src/car_park.py
+++ src/car_park.py
@@ -1,2 +1,19 @@
 class CarPark:
-    pass
+    def __init__(self, location = "undefined",
+                 capacity = 100,
+                 plates = None,
+                 sensors = None,
+                 displays = None):
+        self.location = location
+        self.capacity = capacity
+        self.plates = plates or []
+        self.sensors = sensors or []
+        self.displays = displays or []
+
+    def __str__(self):
+        """
+        Return CarPark object formatted as string.
+        :return: string
+        """
+        # e.g. "Car park at 123 Example Street, with 100 bays.".
+        return f"Car park at {self.location}, with {self.capacity} total bays"
