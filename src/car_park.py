bc1641f4036ddebe221270ac2a1203c9d7fa0f96
mcawezome <20085898@tafe.wa.edu.au>
2024-11-07 14:33:30 +0800

    added stubs to car_park and register function

--- src/car_park.py
+++ src/car_park.py
@@ -1,3 +1,6 @@
+from sensor import Sensor
+from display import Display
+
 class CarPark:
     def __init__(self, location = "undefined",
                  capacity = 100,
@@ -17,3 +20,23 @@ class CarPark:
         """
         # e.g. "Car park at 123 Example Street, with 100 bays.".
         return f"Car park at {self.location}, with {self.capacity} total bays"
+
+    def register(self, component):
+        if type(component) is Sensor:
+            self.sensors.append(component)
+        elif type(component) is Display:
+            self.displays.append(component)
+        else:
+            raise TypeError("Component must be either a Sensor or Display")
+
+    def add_car(self):
+        pass
+
+    def remove_car(self):
+        pass
+
+    def update_displays(self):
+        pass
+
+    def check_plate_in_car_park(self):
+        pass
