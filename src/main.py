06f58f2cf8bc4facdbd36086c7106edd3227fed1
mcawezome <20085898@tafe.wa.edu.au>
2024-11-15 14:42:21 +0800

    Completed TODOs in main

--- src/main.py
+++ src/main.py
@@ -3,17 +3,19 @@ from display import Display
 from car_park import CarPark
 
 if __name__ == "__main__":
-    sensor1 = EntrySensor()
-    sensor2 = ExitSensor()
-    display1 = Display()
-    car_park = CarPark()
+    sensor1 = EntrySensor(1, True)
+    sensor2 = ExitSensor(2, True)
+    display1 = Display(1, "Welcome to Moondalup", True)
+    car_park = CarPark("moondalup", 100, log_file="moondalup.txt")
+
 
     car_park.register(sensor1)
     car_park.register(sensor2)
     car_park.register(display1)
-    car_park.detect_vehicle_entry()
-    car_park.detect_vehicle_entry()
-    car_park.detect_vehicle_entry()
-    car_park.detect_vehicle_entry()
+    for _ in range(10):
+        car_park.detect_vehicle_entry()
+
     car_park.detect_vehicle_exit()
+    car_park.detect_vehicle_exit()
+
     car_park.update_displays()
